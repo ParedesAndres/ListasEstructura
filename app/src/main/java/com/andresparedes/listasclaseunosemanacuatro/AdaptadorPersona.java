5e186d1d798cea92173861046580f467a8b119b4
ParedesAndres <35270955+ParedesAndres@users.noreply.github.com>
2018-02-19 00:55:07 -0500

    Finish

--- app/src/main/java/com/andresparedes/listasclaseunosemanacuatro/AdaptadorPersona.java
+++ app/src/main/java/com/andresparedes/listasclaseunosemanacuatro/AdaptadorPersona.java
@@ -1,9 +1,14 @@
 package com.andresparedes.listasclaseunosemanacuatro;
 
 import android.app.Activity;
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -39,9 +44,29 @@ public class AdaptadorPersona extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
 
-        
+      View v = view;
 
+      if (view == null){
+          LayoutInflater inf = (LayoutInflater) actividad.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+          v = inf.inflate(R.layout.item_persona,null);
+      }
 
-        return null;
+      //Obtengo los datos (nombre, apellido, correo, imagen) desde mi lista
+       Persona personaActual = personas.get(i);
+
+        //Obtengo los Tv del XML
+        TextView nombre = (TextView) v.findViewById(R.id.tv_persona_nombre);
+        TextView apellido = (TextView) v.findViewById(R.id.tv_persona_apellido);
+        TextView correo = (TextView) v.findViewById(R.id.tv_persona_correo);
+
+        //Cambio la info por el modelo Persona
+        nombre.setText(personaActual.getNombre());
+        apellido.setText(personaActual.getApellido());
+        correo.setText(personaActual.getCorreo());
+
+        ImageView img = (ImageView) v.findViewById(R.id.imageView);
+        img.setImageDrawable(personaActual.getImagen());
+
+        return v;
     }
 }
